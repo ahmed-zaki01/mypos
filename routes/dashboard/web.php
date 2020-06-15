521ca417eb12ebdf0b67659a2a1206f26687a44d
ahmed_zaki0263 <ahmed.zaki0263@gmail.com>
2020-06-15 17:51:59 +0200

    add site lang files for arabic and english

--- routes/dashboard/web.php
+++ routes/dashboard/web.php
@@ -4,7 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('dashboard')->name('dashboard.')->group(function () {
 
-    Route::get('/check', function () {
-        return view('dashboard.index');
-    });
+    Route::get('/index', 'DashboardController@index')->name('index');
 });
